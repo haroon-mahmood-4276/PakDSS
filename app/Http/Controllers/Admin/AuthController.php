1d3cf8bd80b0beddc8105e94458b8a9ccff68c5d
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2023-03-23 14:52:44 +0500

    PAK-1:Auth Working

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function loginView(Request $request)
+    {
+        abort_if(request()->ajax(), 403);
 
+        return view('admin.auth.login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        abort_if(request()->ajax(), 403);
+
+        // $waiter = Waiter::where('fSW_Code', $request->waiter)->first();
+        // if ($waiter && $waiter->fSW_PWord == $request->password) {
+        //     Auth::login($waiter);
+        //     $request->session()->regenerate();
+        //     return redirect()->intended(route('dashboard.index'));
+        // }
+
+        return redirect()->route('admin.login.view')->withDanger('Either email or password is wrong!');
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.view');
+    }
 }
