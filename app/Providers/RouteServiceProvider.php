d39bdf832f3925eaf21631cf6d291b4443202cf6
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2023-03-29 00:31:38 +0500

    PAK-3: User Theme installed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/seller/web.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/user/web.php'));
         });
     }
 
---
+++ resources/views/user/layout/datatables/js.blade.php
@@ -0,0 +1,25 @@
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/jquery.dataTables.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-bs5/datatables-bootstrap5.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-responsive/datatables.responsive.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-responsive-bs5/responsive.bootstrap5.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-buttons/datatables-buttons.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-buttons/buttons.html5.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-buttons/buttons.print.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-buttons-bs5/buttons.bootstrap5.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-rowgroup/datatables.rowgroup.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-rowgroup-bs5/rowgroup.bootstrap5.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-checkboxes-jquery/datatables.checkboxes.js"></script>
+{{-- <script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-select/datatables-select.js"></script>
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-select-bs5/select.bootstrap5.js"></script> --}}
+
+
+<script src="{{ asset('admin-assets') }}/vendor/libs/datatables/datatables-buttons/buttons.server-side.js"></script>
+
+
+
+{{--
+    <script src="{{ asset('app-assets') }}/vendors/js/tables/datatable/buttons.colVis.min.js"></script>
+    <script src="{{ asset('app-assets') }}/vendors/js/tables/datatable/jszip.min.js"></script>
+    <script src="{{ asset('app-assets') }}/vendors/js/tables/datatable/pdfmake.min.js"></script>
+    <script src="{{ asset('app-assets') }}/vendors/js/tables/datatable/vfs_fonts.js"></script>
+    --}}
