6d2c938f06af2c06e9daab8572fe66a1ff5a6efa
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2023-05-14 22:21:59 +0500

    PAK-34: Brand page fixed

--- app/Utils/Traits/InterfaceShared.php
+++ app/Utils/Traits/InterfaceShared.php
@@ -4,7 +4,7 @@ namespace App\Utils\Traits;
 
 interface InterfaceShared
 {
-    public function get($relationships = [], $ignore = null, $with_tree = false, $withCount = false);
+    public function get($relationships = [], $ignore = null, $with_tree = false, $withCount = false, $withPagination = false, $perPage = 10);
 
     public function find($id, $relationships = []);
 }
