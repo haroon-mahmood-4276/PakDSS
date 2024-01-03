2c3e9f437e678628fd56567ba7f12c3d69137a76
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2024-01-03 08:08:51 +0500

    PAK-72: Country, State, City Seeder Done

---
+++ app/Models/State.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class State extends Model
+{
+    use HasUuids, HasFactory, SoftDeletes;
+
+    protected $dateFormat = 'U';
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    protected $casts = [];
+
+    public $rules = [];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+}
---
+++ database/migrations/2024_01_03_062221_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('state_id')->constrained();    
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('zip_code')->default(0);
+
+            $table->integer('created_at')->nullable();
+            $table->integer('updated_at')->nullable();
+            $table->integer('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
