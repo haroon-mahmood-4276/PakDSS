274e40a6c8c4c172a9554e88c4decac7c5d93e21
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2024-07-21 19:29:50 +0500

    PAK-87: Prices and Image issue done

--- resources/views/user/layout/layout.blade.php
+++ resources/views/user/layout/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>@yield('page-title') - {{ env('APP_NAME') }}</title>
+    <title>@yield('page-title') - {{ settings('site_name') ?? env('APP_NAME') }}</title>
 
     <meta name="description" content="" />
 
